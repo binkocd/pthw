84f8cf5585ddb58b7aff99af6d5c076b78ba9481
Bailey <jobba@uc81f6610ee18540e20ef.ant.amazon.com>
2015-03-07 18:31:54 -0800

    add 25-29 (27 was memorization)

--- ex24.py
+++ ex24.py
@@ -1,10 +1,10 @@
 print "Let's practice everything."
-print 'You\'d need to know \'bout excapes with \\ that do \n newlines and \tabs.'
+print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
 
 poem = """
-\t The lovely world
+\tThe lovely world
 with logic so firmly planted
-cannot discern \n needs of love
+cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none.
@@ -14,6 +14,7 @@ print "--------------"
 print poem
 print "--------------"
 
+
 five = 10 - 2 + 3 - 6
 print "This should be five: %s" % five
 
@@ -22,7 +23,6 @@ def secret_formula(started):
     jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
-
 
 
 start_point = 10000
