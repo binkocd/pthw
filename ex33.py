37e8f9135cecd9a0ebf69c814f229bb70465ee5f
binkocd <binkocd@gmail.com>
2015-04-08 21:24:24 -0700

    Add ex35

--- ex33.py
+++ ex33.py
@@ -1,7 +1,6 @@
 i = 0
 numbers = []
 
-#def ():
 while i < 6:
     print "At the top i is %d" % i
     numbers.append(i)
@@ -14,3 +13,18 @@ print "The numbers: "
 
 for num in numbers:
     print num
+
+print "Study Drill 1"
+
+def while_loop(limit):
+  i = 0
+  while i < 6:
+    print "At the top i is %d" % i 
+    numbers.append(i)
+
+    i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+limit = 4
+while_loop(limit)
